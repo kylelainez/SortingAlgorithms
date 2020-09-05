7cf13ee2d8ffa29055c2a8c2a2f2da4d402080b4
kylelainez <kyleadrianlainez@gmail.com>
2020-09-04 21:08:17 -0700

    fixed heap sort

--- js/heap_sort.js
+++ js/heap_sort.js
@@ -1,18 +1,56 @@
 const heap = (length,index)=>{
+
+    if (index >= Math.floor(length / 2)) {
+        return;
+      }
+
     let largest = index;
     const left_child = (index * 2) + 1;
-    const right_child = (index * 2  ) + 2;
+    const right_child = (index * 2) + 2 < length ? index*2 + 2: null;
+
+    if (left_child < arraySize) update_element(array[left_child],  arrayHeight[left_child],time,'#ff5976');
+    if (right_child < arraySize && right_child !==null) update_element(array[right_child],  arrayHeight[right_child],time,'#ff5976');
+    update_element(array[index], arrayHeight[index],time,'#ff5976');                    
+    time++;
 
     if(left_child < length && arrayHeight[left_child] > arrayHeight[largest]){
         largest = left_child;
     }
-    if(right_child < length && arrayHeight[right_child] > arrayHeight[largest]){
+    if(right_child < length && arrayHeight[right_child] > arrayHeight[largest] && right_child !== null){
         largest = right_child;
     }
     if(largest !== index){
+        update_element(array[index], arrayHeight[index],time,'red');                    
+        update_element(array[largest],  arrayHeight[largest],time,'red');
+        time++;
+
         swap_elements(index, largest);
+
+        update_element(array[index], arrayHeight[index],time,'red');                    
+        update_element(array[largest],  arrayHeight[largest],time,'red');
+        time++;
+
+        if (left_child < arraySize) update_element(array[left_child],  arrayHeight[left_child],time,'#ff5976');
+        if (right_child < arraySize && right_child !==null) update_element(array[right_child],  arrayHeight[right_child],time,'#ff5976');
+        update_element(array[index], arrayHeight[index],time,'#ff5976');                    
+        time++;
+
+        update_element(array[index], arrayHeight[index],time,'var(--primary-color)');                    
+        update_element(array[left_child],  arrayHeight[left_child],time,'var(--primary-color)');
+        if (right_child < arraySize && right_child !==null)update_element(array[right_child],  arrayHeight[right_child],time,'var(--primary-color)');
+
         heap(length,largest);
     }
+    
+    if (left_child < arraySize) update_element(array[left_child],  arrayHeight[left_child],time,'#ff5976');
+    if (right_child < arraySize && right_child !==null) update_element(array[right_child],  arrayHeight[right_child],time,'#ff5976');
+    update_element(array[index], arrayHeight[index],time,'#ff5976');  
+    time++;
+    
+    if (left_child < arraySize) update_element(array[left_child],  arrayHeight[left_child],time,'var(--primary-color)');
+    if (right_child < arraySize && right_child !==null) update_element(array[right_child],  arrayHeight[right_child],time,'var(--primary-color)');
+    update_element(array[index], arrayHeight[index],time,'var(--primary-color)');                    
+
 };
 
 const heap_sort = () =>{
@@ -22,8 +60,23 @@ const heap_sort = () =>{
     }
 
     for(let i = arraySize - 1; i > 0; i--){
+        update_element(array[0], arrayHeight[0],time,'red');                    
+        update_element(array[i],  arrayHeight[i],time,'red');
+        time++;
+
         swap_elements(0,i);
+
+        update_element(array[0], arrayHeight[0],time,'red');                    
+        update_element(array[i],  arrayHeight[i],time,'red');
+        time++;
+
+        update_element(array[0], arrayHeight[0],time,'green');                    
+        update_element(array[i],  arrayHeight[i],time,'green');
+
         heap(i,0);
     }
 
+    // for(let i = 0; i<arraySize; i++){
+    //     update_element(array[i],  arrayHeight[i],time,'green');
+    // }
 };
